00143c97258d2cbc619ad7e9fd1338ca089ba570
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2023-05-18 16:41:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,205 +1,173 @@
+import tkinter as tk
+from tkinter import ttk
 import random
 import time
-import tkinter as tk
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# Sıralama işlemlerini başlatma
-# Sıralama işlemlerini başlatma
-def start_sorting():
-    # Kullanıcının seçtiği değerleri al
-    size = int(entry_size.get())
-    manual = checkbox_manual_var.get()
-    algorithm = algorithm_var.get()  # Retrieve the selected algorithm
-    graph_type = graph_type_var.get()
-    speed = float(entry_speed.get())
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                update_display(arr)
+                time.sleep(speed_slider.get())
 
-    # Sıralama işlemlerini başlat
-    lst = create_list(size, manual)
-    animate_sorting(lst, algorithm, graph_type, speed)
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi-1)
+        quick_sort(arr, pi+1, high)
 
-
-# Liste oluşturma
-def create_list(size, manual=False):
-    if manual:
-        # Manual giriş yapılıyorsa, kullanıcıdan liste elemanlarını alınır
-        input_window = tk.Toplevel()
-        input_window.title("Liste Girişi")
-
-        elements_label = tk.Label(input_window, text="Liste Elemanlarını Virgülle Ayırarak Girin:")
-        elements_label.pack()
-        elements_entry = tk.Entry(input_window)
-        elements_entry.pack()
-
-        confirm_button = tk.Button(input_window, text="Onayla", command=lambda: input_window.destroy())
-        confirm_button.pack()
-
-        input_window.wait_window(input_window)
-        elements = elements_entry.get().split(",")
-        lst = [int(element.strip()) for element in elements]
-    else:
-        # Rastgele liste oluşturulur
-        lst = [random.randint(1, 100) for _ in range(size)]
-    return lst
-
-
-# Sıralama animasyonunu gerçekleştirme
-def animate_sorting(lst, algorithm, graph_type, speed):
-    # Grafik arayüzünü oluşturma
-    fig = Figure(figsize=(5, 4), dpi=100)
-    canvas = FigureCanvasTkAgg(fig, master=tk.Toplevel())
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-    ax = fig.add_subplot(111)
-
-    # Sıralama işlemini başlatma
-    if algorithm == "Selection Sort":
-        selection_sort(lst, ax, speed, canvas)
-    elif algorithm == "Bubble Sort":
-        bubble_sort(lst, ax, speed, canvas)
-    elif algorithm == "Insertion Sort":
-        insertion_sort(lst, ax, speed, canvas)
-    elif algorithm == "Merge Sort":
-        merge_sort(lst, ax, speed, canvas)
-    elif algorithm == "Quick Sort":
-        quick_sort(lst, ax, speed, canvas)
-
-    # Sıralanmış liste ve karşılaştırma sayısı yazdırma
-    print("Sıralanmış Liste: ", lst)
-    print("Karşılaştırma Sayısı: ", len(lst) * (len(lst) - 1) // 2)
-
-# Seçme Sıralaması (Selection Sort)
-def selection_sort(lst, ax, speed, canvas):
-    for i in range(len(lst)):
-        min_idx = i
-        for j in range(i + 1, len(lst)):
-            if lst[min_idx] > lst[j]:
-                min_idx = j
-        lst[i], lst[min_idx] = lst[min_idx], lst[i]
-        ax.clear()
-        ax.bar(range(len(lst)), lst, color='b')
-        canvas.draw_idle()
-        time.sleep(speed)
-# Kabarcık Sıralaması (Bubble Sort)
-def bubble_sort(lst, ax, speed, canvas):
-    n = len(lst)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if lst[j] > lst[j + 1]:
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
-                ax.clear()
-                ax.bar(range(len(lst)), lst, color='b')
-                canvas.draw_idle()
-                time.sleep(speed)
-
-# Ekleme Sıralaması (Insertion Sort)
-def insertion_sort(lst, ax, speed, canvas):
-    for i in range(1, len(lst)):
-        key = lst[i]
-        j = i - 1
-        while j >= 0 and key < lst[j]:
-            lst[j + 1] = lst[j]
-            j -= 1
-        lst[j + 1] = key
-        ax.clear()
-        ax.bar(range(len(lst)), lst, color='b')
-        canvas.draw_idle()
-        time.sleep(speed)
-
-# Birleştirme Sıralaması (Merge Sort)
-def merge_sort(lst, ax, speed, canvas):
-    if len(lst) > 1:
-        mid = len(lst) // 2
-        left_half = lst[:mid]
-        right_half = lst[mid:]
-        merge_sort(left_half, ax, speed, canvas)
-        merge_sort(right_half, ax, speed, canvas)
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                lst[k] = left_half[i]
-                i += 1
-            else:
-                lst[k] = right_half[j]
-                j += 1
-            k += 1
-        while i < len(left_half):
-            lst[k] = left_half[i]
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
             i += 1
-            k += 1
-        while j < len(right_half):
-            lst[k] = right_half[j]
-            j += 1
-            k += 1
-        ax.clear()
-        ax.bar(range(len(lst)), lst, color='b')
-        canvas.draw_idle()
-        time.sleep(speed)
+            arr[i], arr[j] = arr[j], arr[i]
+            update_display(arr)
+            time.sleep(speed_slider.get())
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
 
-# Hızlı Sıralama (Quick Sort)
-def quick_sort(lst, ax, speed, canvas):
-    if len(lst) <= 1:
-        return lst
-    else:
-        pivot = lst[0]
-        left = []
-        right = []
-        for i in range(1, len(lst)):
-            if lst[i] < pivot:
-                left.append(lst[i])
-            else:
-                right.append(lst[i])
-        ax.clear()
-        ax.bar(range(len(left)), left, color='r')
-        ax.bar(len(left), pivot, color='g')
-        ax.bar(range(len(left) + 1, len(lst)), right, color='r')
-        canvas.draw_idle()
-        time.sleep(speed)
-        return quick_sort(left, ax, speed, canvas) + [pivot] + quick_sort(right, ax, speed, canvas)
+def update_display(arr):
+    canvas.delete("all")
+    bar_width = canvas_width // len(arr)
+    bar_height_ratio = canvas_height / max(arr)
+    for i, value in enumerate(arr):
+        x0 = i * bar_width
+        y0 = canvas_height - value * bar_height_ratio
+        x1 = (i + 1) * bar_width
+        y1 = canvas_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill='sky blue', outline='white')
+    window.update_idletasks()
 
-# Tkinter penceresini oluştur
+def create_array():
+    size = int(size_spinbox.get())
+    elements = list(map(int, list_text.get("1.0", tk.END).split()))
+    return elements[:size]
+
+def create_graph():
+    graph_type = graph_combo.get()
+    array = create_array()
+    if graph_type == 'Scatter':
+        scatter_graph(array)
+    elif graph_type == 'Bar':
+        bar_graph(array)
+    elif graph_type == 'Stem':
+        stem_graph(array)
+
+def scatter_graph(arr):
+    canvas.delete("all")
+    x_interval = canvas_width / len(arr)
+    y_interval = canvas_height / max(arr)
+    for i, value in enumerate(arr):
+        x = i * x_interval
+        y = canvas_height - value * y_interval
+        canvas.create_oval(x, y, x, y, width=3, fill='sky blue')
+    window.update_idletasks()
+
+def bar_graph(arr):
+    canvas.delete("all")
+    bar_width = canvas_width // len(arr)
+    bar_height_ratio = canvas_height / max(arr)
+    for i, value in enumerate(arr):
+        x0 = i * bar_width
+        y0 = canvas_height - value * bar_height_ratio
+        x1 = (i + 1) * bar_width
+        y1 = canvas_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill='sky blue', outline='white')
+    window.update_idletasks()
+
+def stem_graph(arr):
+    canvas.delete("all")
+    x_interval = canvas_width / len(arr)
+    y_interval = canvas_height / max(arr)
+    for i, value in enumerate(arr):
+        x = i * x_interval
+        y = canvas_height - value * y_interval
+        canvas.create_line(x, canvas_height, x, y, width=2)
+    window.update_idletasks()
+
+def start_animation():
+    selected_algorithm = algorithm_combo.get()
+    array = create_array()
+    create_graph()
+    if selected_algorithm == 'Bubble Sort':
+        bubble_sort(array)
+    elif selected_algorithm == 'Quick Sort':
+        quick_sort(array, 0, len(array)-1)
+    # TODO: Start the animation
+
+def stop_animation():
+    # TODO: Stop the animation
+    pass
+
+def reset():
+    canvas.delete("all")
+    size_spinbox.delete(0, tk.END)
+    list_text.delete("1.0", tk.END)
+
 window = tk.Tk()
+window.title('Sıralama Algoritması Paneli')
+window.geometry('1100x500')
 
-# Kullanıcı seçimlerini yapabileceği bileşenler
-label_size = tk.Label(window, text="Liste Boyutu:")
-entry_size = tk.Entry(window)
+canvas_width = 800
+canvas_height = 400
 
-checkbox_manual_var = tk.BooleanVar()  # Define the checkbox_manual_var variable
-checkbox_manual = tk.Checkbutton(window, text="Manuel Giriş", variable=checkbox_manual_var)
+canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, bg='white')
+canvas.pack(side=tk.LEFT, padx=20, pady=20)
 
-algorithm_var = tk.StringVar()  # Create a StringVar for the algorithm selection
-algorithm_var.set("Selection Sort")  # Varsayılan olarak Seçme Sıralaması seçili
-label_algorithm = tk.Label(window, text="Sıralama Algoritması:")
-dropdown_algorithm = tk.OptionMenu(window, algorithm_var, "Selection Sort", "Bubble Sort", "Insertion Sort", "Merge Sort", "Quick Sort")
+settings_frame = tk.Frame(window)
+settings_frame.pack(side=tk.RIGHT, padx=20, pady=20)
 
-label_graph_type = tk.Label(window, text="Grafik Türü:")
-graph_type_var = tk.StringVar()
-graph_type_var.set("Bar") # Varsayılan olarak Bar grafiği seçili
-dropdown_graph_type = tk.OptionMenu(window, graph_type_var, "Bar", "Line", "Scatter")
+# Size and List Input Section
+size_label = tk.Label(settings_frame, text='Liste Boyutu:', font=('Arial', 12))
+size_label.pack()
 
-label_speed = tk.Label(window, text="Hız (saniye):")
-entry_speed = tk.Entry(window)
-entry_speed.insert(tk.END, "0.5") # Varsayılan olarak 0.5 saniye
+size_spinbox = tk.Spinbox(settings_frame, from_=1, to=100, font=('Arial', 12))
+size_spinbox.pack()
 
-start_button = tk.Button(window, text="Sıralamayı Başlat", command=start_sorting)
+list_label = tk.Label(settings_frame, text='Liste Elemanları:', font=('Arial', 12))
+list_label.pack()
 
-# Bileşenleri pencereye yerleştirme
-label_size.pack()
-entry_size.pack()
+list_text = tk.Text(settings_frame, height=4, width=20, font=('Arial', 12))
+list_text.pack()
 
-checkbox_manual.pack()
+# Speed Setting Section
+speed_label = tk.Label(settings_frame, text='Sıralama Hızı:', font=('Arial', 12))
+speed_label.pack()
 
-label_algorithm.pack()
-dropdown_algorithm.pack()
+speed_slider = ttk.Scale(settings_frame, from_=0.1, to=1.0, length=200, orient='horizontal')
+speed_slider.pack()
 
-label_graph_type.pack()
-dropdown_graph_type.pack()
+# Sorting Algorithms Section
+algorithm_label = tk.Label(settings_frame, text='Sıralama Algoritması:', font=('Arial', 12))
+algorithm_label.pack()
 
-label_speed.pack()
-entry_speed.pack()
+algorithm_combo = ttk.Combobox(settings_frame, values=['Bubble Sort', 'Quick Sort'], font=('Arial', 12))
+algorithm_combo.current(0)
+algorithm_combo.pack()
 
+# Graph Types Section
+graph_label = tk.Label(settings_frame, text='Grafik Tipi:', font=('Arial', 12))
+graph_label.pack()
+
+graph_combo = ttk.Combobox(settings_frame, values=['Scatter', 'Bar', 'Stem'], font=('Arial', 12))
+graph_combo.current(0)
+graph_combo.pack()
+
+# Create, Start, Stop, Reset Buttons
+create_button = ttk.Button(settings_frame, text='Oluştur', command=create_graph)
+create_button.pack()
+
+start_button = ttk.Button(settings_frame, text='Başlat', command=start_animation)
 start_button.pack()
 
-# Pencereyi çalıştırma
+stop_button = ttk.Button(settings_frame, text='Durdur', command=stop_animation)
+stop_button.pack()
+
+reset_button = ttk.Button(settings_frame, text='Sıfırla', command=reset)
+reset_button.pack()
+
 window.mainloop()
-
-
